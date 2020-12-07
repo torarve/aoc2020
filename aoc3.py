7dfb13f07720db1fcd00754b285a9dd395177411
Tor Arve Stangeland <tor.arve.stangeland@gmail.com>
2020-12-07 19:48:09 +0000

    Clean up day 3 code.

--- aoc3.py
+++ aoc3.py
@@ -1,43 +1,29 @@
 import itertools
 
-
 def read_input():
     with open("input3.txt") as file:
         return [x.strip() for x in file.readlines()]
 
-lines = read_input()
-
-def try1():
-    line_size = max(map(lambda x: len(x), lines))
-    num_lines = len(lines)
-    positions = [(x*3)%line_size for x in range(0, num_lines-1)]
-
-    #print(line_size)
-    #print(positions)
-    # print(lines)
-    squares = list(map(lambda pos, line: line[pos], positions, lines))
-    #print(squares)
-    hits = list(filter(lambda x: x=="#", squares))
-
-    print(len(hits))
-
-def get_square(pos, line):
+def get_at(pos, line):
     pos = pos % len(line)
     return line[pos]
 
 def is_tree(square):
     return square=="#"
 
-def number_of_hits(step_x, step_y=1):
-    squares = map(get_square, itertools.count(step=step_x), lines[::step_y])
+def number_of_collisions(step_x, step_y=1):
+    lines = read_input()
+    squares = map(get_at, itertools.count(step=step_x), lines[::step_y])
     hits = filter(is_tree, squares)
     return len(list(hits))
 
-print(number_of_hits(3))
+print(f"Answer to part 1: {number_of_collisions(3)}")
 
-a, b, c, d, e = number_of_hits(1), number_of_hits(3), number_of_hits(5), number_of_hits(7), number_of_hits(1,2)
+a, b, c, d, e = (
+    number_of_collisions(1),
+    number_of_collisions(3),
+    number_of_collisions(5),
+    number_of_collisions(7),
+    number_of_collisions(1,2))
 
-print(a,b,c,d,e)
-print(a*b*c*d*e)
-
-# ".#.#....##.......#..........#.."
+print(f"Answer to part 2: {a*b*c*d*e}")

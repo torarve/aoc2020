c93faa1759ea5330f88a7afc18bcea9851b6c108
Tor Arve Stangeland <tor.arve.stangeland@gmail.com>
2020-12-08 19:50:23 +0100

    Clean up - remove commented out code - explanation of output

--- aoc5.py
+++ aoc5.py
@@ -6,19 +6,11 @@ def to_bin(seat):
     value = seat.replace("F", "0").replace("B", "1").replace("R", "1").replace("L", "0")
     return int(value, 2)
 
-# print(to_bin("BFFFBBFRRR"))
-
-
 seats = list([to_bin(x) for x in read_input()])
 highest = max(seats)
 lowest = min(seats)
-print(highest)
+print(f"Higest seat ID: {highest}")
 
 for i in range(lowest, highest):
     if i not in seats:
-        print(i)
-
-#for x in sorted(seats):
-#    print(x)
-
-# print(653 in seats)
+        print(f"ID of my seat: {i}")

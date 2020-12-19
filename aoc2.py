36d756379f01d370c5f4641ae469b67efa21e6bb
Tor Arve Stangeland <tor.arve.stangeland@gmail.com>
2020-12-19 12:00:40 +0100

    Clean up solution to day 2.

--- aoc2.py
+++ aoc2.py
@@ -1,11 +1,8 @@
 import re
 
-def is_valid_part2(line):
-    m = re.match("(\\d+)-(\\d+) (\\w): (.+)", line)
-    first, second, char, password = m.groups()
-    first_pos = int(first) - 1
-    second_pos = int(second) - 1
-    return (password[first_pos]==char) ^ (password[second_pos]==char)
+def read_input():
+    with open("input2.txt") as file:
+        return [x.strip() for x in file.readlines()]
 
 def is_valid_part1(line):
     m = re.match("(\\d+)-(\\d+) (\\w): (.+)", line)
@@ -13,12 +10,17 @@ def is_valid_part1(line):
     count = password.count(char)
     return count>=int(lower) and count <= int(upper)
 
-lines = None
-with open("input2.txt") as file:
-    lines = [x.strip() for x in file.readlines()]
+def is_valid_part2(line):
+    m = re.match("(\\d+)-(\\d+) (\\w): (.+)", line)
+    first, second, char, password = m.groups()
+    first_pos = int(first) - 1
+    second_pos = int(second) - 1
+    return (password[first_pos]==char) ^ (password[second_pos]==char)
 
-lines = [x for x in lines if is_valid_part1(x)]
-print(len(lines))
+lines = read_input()
 
-lines = [x for x in lines if is_valid_part2(x)]
-print(len(lines))
+valid_passwords_part1 = [x for x in lines if is_valid_part1(x)]
+print(f"Valid passwords (part 1): {len(valid_passwords_part1)}")
+
+valid_passwords_part2 = [x for x in lines if is_valid_part2(x)]
+print(f"Valid passwords (part 2): {len(valid_passwords_part2)}")
